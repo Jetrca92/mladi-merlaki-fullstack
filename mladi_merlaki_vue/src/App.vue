dcbab2c1e0fa073120fcd71034caaebf8bca5f33
Jetrca92 <blaz.guzej@gmail.com>
2024-03-20 21:11:41 +0100

    Complete landing page

--- mladi_merlaki_vue/src/App.vue
+++ mladi_merlaki_vue/src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    
+  </div>
+  
+  
+  <HomeView />
+  
+
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2024 @Jetrca92</p>
+  </footer>
 </template>
 
+<script setup>
+import NavBar from './components/NavBar.vue';
+import HomeView from './views/HomeView.vue';
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma/';
 </style>
