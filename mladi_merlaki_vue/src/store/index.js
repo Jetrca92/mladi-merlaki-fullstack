672a0fb7e772fe9c3764a88e1765f528c9a64844
Jetrca92 <blaz.guzej@gmail.com>
2024-04-11 11:10:39 +0200

    Update app_info once every hour

--- mladi_merlaki_vue/src/store/index.js
+++ mladi_merlaki_vue/src/store/index.js
@@ -1,12 +1,15 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
 
+const HOUR_IN_MS = 3600000
 
 export default createStore({
   state: {
     isAuthenticated: false,
     token: '',
-    portfolio: null
+    portfolio: null,
+    appInfo: null,
+    lastAppInfoFetchTime: null,
   },
   getters: {
   },
@@ -30,7 +33,13 @@ export default createStore({
     },
     setPortfolio(state, portfolio) {
       state.portfolio = portfolio
-    }
+    },
+    setAppInfo(state, appInfo) {
+      state.appInfo = appInfo
+    },
+    setLastAppInfoFetchTime(state, time) {
+      state.lastAppInfoFetchTime = time;
+    },
   },
   actions: {
     initializeStore({ commit} ) {
@@ -42,9 +51,25 @@ export default createStore({
         commit('setPortfolio', response.data)
       } catch (error) {
         console.error(error)
-        // Handle error as needed
       }
-    }
+    },
+    async fetchAppInfo({ commit, state }) {
+      // Check if enough time has passed since the last API call
+      if (!state.lastAppInfoFetchTime || Date.now() - state.lastAppInfoFetchTime >= HOUR_IN_MS) {
+        try {
+          const response = await axios.get('api/v1/marketdata/app_data/')
+          commit('setAppInfo', response.data)
+          commit('setLastAppInfoFetchTime', Date.now()); // Update last call timestamp
+          return response.data;
+        } catch (error) {
+          console.error('Error fetching app info:', error)
+          throw error;
+        }
+      } else {
+        // If less than an hour has passed, return the cached data
+        return state.appInfo;
+      }
+    },
   },
   modules: {
   }
